f98a7e3ec6c83bc8c98b723610d4e0bdaf777f6f
md monjur bakth mazumder <md.monjurmbm2001@gmail.com>
2023-11-25 18:27:58 +0600

    add make admin and make premium user funtionality

--- src/Pages/Dashboard/DashboardAdminPages/ManageUsers/ManageUserRow.jsx
+++ src/Pages/Dashboard/DashboardAdminPages/ManageUsers/ManageUserRow.jsx
@@ -1,7 +1,68 @@
 import PropTypes from "prop-types";
+import useAxiosSecure from "../../../../Hock/useAxiosSecure";
+import Swal from "sweetalert2";
 
 const ManageUserRow = ({ item }) => {
   const { name, email, role, premium } = item || {};
+  const axiosSecure = useAxiosSecure();
+  const handleMakeAdmin = () => {
+    const admin = {
+      name,
+      email,
+      role: "admin",
+      premium,
+    };
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You want make this user as admin?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes,make admin!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.put("/users/make-admin", admin).then((res) => {
+          if (res.status === 200) {
+            Swal.fire({
+              title: "Make admin successfully ",
+              text: "This user is admin from now",
+              icon: "success",
+            });
+          }
+        });
+      }
+    });
+  };
+  const handleMakePremiumUser = () => {
+    const premiumUser = {
+      name,
+      email,
+      role,
+      premium: true,
+    };
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You want make this user as premium?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes,make premium!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.put("/users/make-premium-user", premiumUser).then((res) => {
+          if (res.status === 200) {
+            Swal.fire({
+              title: "Premium user successful",
+              text: "This user is premium user from now",
+              icon: "success",
+            });
+          }
+        });
+      }
+    });
+  };
   return (
     <tr>
       <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 ">
@@ -19,7 +80,10 @@ const ManageUserRow = ({ item }) => {
             Admin
           </button>
         ) : (
-          <button className="py-2 px-5 inline-flex justify-center items-center gap-2 rounded font-semibold border border-pink-600 bg-transparent text-pink-500 hover:bg-pink-600 hover:text-white">
+          <button
+            onClick={handleMakeAdmin}
+            className="py-2 px-5 inline-flex justify-center items-center gap-2 rounded font-semibold border border-pink-600 bg-transparent text-pink-500 hover:bg-pink-600 hover:text-white"
+          >
             User
           </button>
         )}
@@ -33,7 +97,10 @@ const ManageUserRow = ({ item }) => {
             Premium
           </button>
         ) : (
-          <button className="py-2 px-5 inline-flex justify-center items-center gap-2 rounded font-semibold border border-pink-600 bg-transparent text-pink-500 hover:bg-pink-600 hover:text-white">
+          <button
+            onClick={handleMakePremiumUser}
+            className="py-2 px-5 inline-flex justify-center items-center gap-2 rounded font-semibold border border-pink-600 bg-transparent text-pink-500 hover:bg-pink-600 hover:text-white"
+          >
             Make Premium
           </button>
         )}
