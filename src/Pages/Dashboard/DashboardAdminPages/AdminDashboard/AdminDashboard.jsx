da42ae6bd64fd3ee9e4e19c617b96e8927e47b6f
md monjur bakth mazumder <md.monjurmbm2001@gmail.com>
2023-11-27 18:46:08 +0600

    update ui in admin deshboard

--- src/Pages/Dashboard/DashboardAdminPages/AdminDashboard/AdminDashboard.jsx
+++ src/Pages/Dashboard/DashboardAdminPages/AdminDashboard/AdminDashboard.jsx
@@ -33,39 +33,39 @@ const AdminDashboard = () => {
         </h1>
         <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5">
           <div className="border border-pink-500 rounded text-center p-5 md:p-10">
-            <h1 className="text-xl font-bold">Total biodatas</h1>
             <h1 className="text-6xl font-bold text-pink-500">{totalBiodata}</h1>
+            <h1 className="text-xl font-bold">Total biodatas</h1>
           </div>
           <div className="border border-pink-500 rounded text-center p-5 md:p-10">
-            <h1 className="text-xl font-bold">Total male biodatas</h1>
             <h1 className="text-6xl font-bold text-pink-500">
               {" "}
               {totalMaleBiodata}
             </h1>
+            <h1 className="text-xl font-bold">Total male biodatas</h1>
           </div>
           <div className="border border-pink-500 rounded text-center p-5 md:p-10">
-            <h1 className="text-xl font-bold">Total female biodatas</h1>
             <h1 className="text-6xl font-bold text-pink-500">
               {" "}
               {totalFemaleBiodata}
             </h1>
+            <h1 className="text-xl font-bold">Total female biodatas</h1>
           </div>
           <div className="border border-pink-500 rounded text-center p-5 md:p-10">
-            <h1 className="text-xl font-bold">Total premium biodatas</h1>
             <h1 className="text-6xl font-bold text-pink-500">
               {totalPremiumBiodata}
             </h1>
+            <h1 className="text-xl font-bold">Total premium biodatas</h1>
           </div>
           <div className="border border-pink-500 rounded text-center p-5 md:p-10">
-            <h1 className="text-xl font-bold">Total revenue</h1>
             <h1 className="text-6xl font-bold text-pink-500">
               {totalRevenue}
               <span className="text-5xl">&#2547;</span>
             </h1>
+            <h1 className="text-xl font-bold">Total revenue</h1>
           </div>
           <div className="border border-pink-500 rounded text-center p-5 md:p-10">
-            <h1 className="text-xl font-bold">Total User</h1>
             <h1 className="text-6xl font-bold text-pink-500">{totalUser}</h1>
+            <h1 className="text-xl font-bold">Total User</h1>
           </div>
         </div>
         <Chart pieChartData={pieChartData} />
