a1979e2d94d6b0559a110f4ad3dfb9d556c90bb1
md monjur bakth mazumder <md.monjurmbm2001@gmail.com>
2023-11-28 02:33:58 +0600

    update

--- src/Pages/Home/SuccessStory/SuccessStory.jsx
+++ src/Pages/Home/SuccessStory/SuccessStory.jsx
@@ -4,11 +4,11 @@ import useSuccessStory from "../../../Hock/useSuccessStory";
 import SuccessStoryCard from "./SuccessStoryCard";
 
 const SuccessStory = () => {
-    const successStory =  useSuccessStory()
-    console.log("successStory",successStory);
+  const successStory = useSuccessStory();
+  console.log("successStory", successStory);
   return (
     <Container>
-      <Heading>Success Story {successStory?.length}</Heading>
+      <Heading>Success Story</Heading>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mb-5 md:mb-10">
         {successStory?.map((item, i) => (
           <SuccessStoryCard key={i} item={item} />
