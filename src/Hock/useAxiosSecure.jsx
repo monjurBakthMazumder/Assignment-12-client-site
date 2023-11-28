356eb08fa5c9ffadafdb072b9425ba7ae1d5a898
md monjur bakth mazumder <md.monjurmbm2001@gmail.com>
2023-11-28 14:34:56 +0600

    deploy

--- src/Hock/useAxiosSecure.jsx
+++ src/Hock/useAxiosSecure.jsx
@@ -3,28 +3,29 @@ import axios from "axios";
 import { useEffect } from "react";
 
 const axiosSecure = axios.create({
-    baseURL: 'http://localhost:5000',
-    withCredentials: true,
-  });
-  const useAxiosSecure = () => {
-    // const { logoutUser } = UseAuth();
-  
-    useEffect(() => {
-      axiosSecure.interceptors.response.use(
-        (res) => {
-          return res;
-        },
-        async (error) => {
-          const status = error.response.status;
-          if (status === 401 || status === 403) {
-              // await logoutUser();
-              // navigate('/login');
-          }
+  // baseURL: 'http://localhost:5000',
+  baseURL: "https://mingle-heart.vercel.app",
+  withCredentials: true,
+});
+const useAxiosSecure = () => {
+  // const { logoutUser } = UseAuth();
+
+  useEffect(() => {
+    axiosSecure.interceptors.response.use(
+      (res) => {
+        return res;
+      },
+      async (error) => {
+        const status = error.response.status;
+        if (status === 401 || status === 403) {
+          // await logoutUser();
+          // navigate('/login');
         }
-      );
-    }, []);
-  
-    return axiosSecure;
-  };
-  
-  export default useAxiosSecure;
+      }
+    );
+  }, []);
+
+  return axiosSecure;
+};
+
+export default useAxiosSecure;
