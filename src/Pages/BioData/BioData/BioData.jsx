6b122ece5311a11dfa5804a1f8e1aa48e31ece86
md monjur bakth mazumder <md.monjurmbm2001@gmail.com>
2023-11-29 19:52:53 +0600

    add pagination

--- src/Pages/BioData/BioData/BioData.jsx
+++ src/Pages/BioData/BioData/BioData.jsx
@@ -1,7 +1,7 @@
 import FilterOption from "./filterOption";
 import BioDataCard from "./BioDataCard";
 import Container from "../../../Component/Ui/Container";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import useAxiosPublic from "../../../Hock/useAxiosPublic";
 import Heading from "../../../Component/Ui/Heading";
 import { useQuery } from "@tanstack/react-query";
@@ -11,20 +11,44 @@ import { Helmet } from "react-helmet";
 const BioData = () => {
   const axiosPublic = useAxiosPublic();
   const [filterOption, setFilterOption] = useState({});
+  const itemPerPage = 20;
+  const [currentPage, setCurrentPage] = useState(0);
+  const [count, setCount] = useState(null);
+  const [loading, setLoading] = useState(true);
 
+  useEffect(()=>{
+    setLoading(true)
+    axiosPublic.get("/all-bioData-count").then((res) => {
+      setCount(res.data.result);
+      setLoading(false)
+    });
+  },[axiosPublic])
+  console.log("count", count);
   const {
     data: bioData = [],
     isPending: isPendingBioData,
-    refetch: refetchBioData,
   } = useQuery({
-    queryKey: ["all-bioData", filterOption],
+    queryKey: ["all-bioData", filterOption, currentPage, itemPerPage,count],
     queryFn: async () => {
       const res = await axiosPublic.get(
-        `/all-bioData?ageTo=${filterOption?.ageTo}&ageFrom=${filterOption?.ageFrom}&gender=${filterOption?.gender}&permanentDivision=${filterOption?.permanentDivision}&presentDivision=${filterOption?.presentDivision}`
+        `/all-bioData?ageTo=${filterOption?.ageTo}&ageFrom=${filterOption?.ageFrom}&gender=${filterOption?.gender}&permanentDivision=${filterOption?.permanentDivision}&presentDivision=${filterOption?.presentDivision}&page=${currentPage}&size=${itemPerPage}`
       );
       return res.data;
     },
   });
+  const numberOfPages = Math?.ceil(count / itemPerPage);
+  const pages = [...Array(numberOfPages).keys()];
+
+  const handlePrevPage = () => {
+    if (currentPage > 0) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+  const handleNextPage = () => {
+    if (currentPage < pages?.length - 1) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
 
   return (
     <>
@@ -32,22 +56,49 @@ const BioData = () => {
         <title>MingleHeart | Biodatas</title>
       </Helmet>
       <Container>
-        <Heading>All Biodatas: {bioData?.length}</Heading>
+        <Heading>All Biodatas</Heading>
         <div className="flex flex-col sm:flex-row gap-5">
           <div className="sm:w-60">
             <FilterOption setFilterOption={setFilterOption} />
           </div>
-          {isPendingBioData ? (
+          {isPendingBioData || loading ?  (
             <Loading />
           ) : (
             <div className="flex-1">
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+              <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5">
                 {bioData.map((item) => (
                   <BioDataCard key={item._id} item={item} />
                 ))}
               </div>
             </div>
           )}
+        </div>
+        <div className="flex flex-wrap justify-center items-center gap-2 my-10">
+          <button
+            onClick={handlePrevPage}
+            className={`px-2 py-1 border border-pink-600 flex justify-center items-center gap-2 hover:bg-pink-600 hover:text-white text-pink-600 bg-transparent`}
+          >
+            Prev
+          </button>
+          {pages?.map((page, i) => (
+            <button
+              key={page}
+              onClick={() => setCurrentPage(page)}
+              className={`px-2 py-1 border border-pink-600 flex justify-center items-center gap-2 hover:bg-pink-600 hover:text-white ${
+                currentPage === page
+                  ? "bg-pink-600 text-white"
+                  : "text-pink-600 bg-transparent"
+              }`}
+            >
+              {++i}
+            </button>
+          ))}
+          <button
+            onClick={handleNextPage}
+            className={`px-2 py-1 border border-pink-600 flex justify-center items-center gap-2 hover:bg-pink-600 hover:text-white text-pink-600 bg-transparent`}
+          >
+            Next
+          </button>
         </div>
       </Container>
     </>
