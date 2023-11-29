c088b0b29da45fe716ab47b9991a9d05e6da5fa3
md monjur bakth mazumder <md.monjurmbm2001@gmail.com>
2023-11-29 21:25:16 +0600

    update success story ui

--- src/Pages/Dashboard/DashboardPages/GotMarried/GotMarried.jsx
+++ src/Pages/Dashboard/DashboardPages/GotMarried/GotMarried.jsx
@@ -33,8 +33,12 @@ const GotMarried = () => {
                 )}
               </div>
             ) : (
-              <div className="flex flex-col justify-center items-center gap-5 h-screen text-3xl md:text-4xl lg:text-5xl font-semibold">
-                <h1>No Biodata added</h1>
+              <div className="flex flex-col justify-center items-center gap-5 h-screen text-xl md:text-2xl lg:text-3xl font-semibold w-4/5 mx-auto">
+                <h1 className="text-center">
+                  You have no biodata added <br />
+                  If you want to post got marriage success story, you must
+                  create a biodata
+                </h1>
                 <Link
                   to={"/dashboard/edit-bioData"}
                   className="py-3 text-sm px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-pink-500 text-white hover:bg-pink-600 focus:outline-none focus:ring-2 focus:ring-pink-500"
